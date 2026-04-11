5b47bcb6835025a1b6bfbee1b4646cceea59c33e
dev Ev Green <dev@greenhproject.com>
2026-04-11 11:16:13 -0500

    Add capacitor projecto configuration

---
+++ android/app/src/main/java/com/evgreen/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.evgreen.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.evgreen.app',
+  appName: 'Green EV Platform',
+  webDir: 'dist/public'
+};
+
+export default config;
