7f48da24b5b9269665fec06c6ca1287bacaa07f9
Leonardo López <leonardolopezp92@gmail.com>
2026-06-14 08:54:25 -0500

    codemagic configuration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,15 +2,20 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.greenhproject.evgreen',
-  appName: 'Green EV Platform',
-  webDir: 'dist/public',
+  appName: 'Evgreen',
+  webDir: 'dist/public', // Sincronizado con vite.config.ts
   server: {
-    androidScheme: 'https'
+    androidScheme: 'https',
+    iosScheme: 'evgreen' // Esquema para que Auth0 pueda volver a la app en iOS
   },
   plugins: {
     CapacitorHttp: {
       enabled: true
     }
+  },
+  ios: {
+    path: 'ios',
+    handleApplicationNotifications: true
   }
 };
 
